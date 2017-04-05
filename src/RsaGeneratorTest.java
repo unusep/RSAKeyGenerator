87d2f61980c773221215a5c93734b8c5e45dde27
Deshun Cai <unusep@gmail.com>
2017-04-06 01:54:31 +0800

    add test case to check keys generated are of desired length

--- src/RsaGeneratorTest.java
+++ src/RsaGeneratorTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import java.math.BigInteger;
 import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import javafx.util.Pair;
@@ -15,15 +16,36 @@ public class RsaGeneratorTest {
             return b;
         }
     }
+
+    IRandomNumberGenerator ranGen;
     
+    @Before
+    public void initObjects() {
+        ranGen = new TestGenerator();
+    }
+    
+    // naively checks if the keys generated are probably prime
     @Test
     public void GeneratesRsaKeys() {
-        IRandomNumberGenerator ranGen = new TestGenerator();
         IRsaGenerator RsaGen = new HomeworkRsaGenerator(ranGen);
         Pair<BigInteger, BigInteger> keys = RsaGen.generateKeys();
         assertTrue(keys != null);
         assertTrue(keys.getKey().isProbablePrime(1));
         assertTrue(keys.getValue().isProbablePrime(1));
+    }
+    
+    @Test
+    public void KeysAreWithinRange() {
+        int numBits = 128;
+        int accuracy = 1;
+        IRsaGenerator RsaGen = new HomeworkRsaGenerator(ranGen, numBits, accuracy);
+        Pair<BigInteger, BigInteger> keys = RsaGen.generateKeys();
+
+        BigInteger LOWER = BigInteger.valueOf(2).pow(numBits - 1);
+        BigInteger UPPER = BigInteger.valueOf(2).pow(numBits).subtract(BigInteger.ONE);
+        
+        assertTrue(keys.getKey().compareTo(LOWER) >= 1);
+        assertTrue(keys.getValue().compareTo(UPPER) <= 1);
     }
     
 }
